79e61e752cb2dee6919b907f5ce26a5ff506b38e
Vladislav Kotletkin <drzhkn1@mail.ru>
2026-02-03 23:44:36 +0300

    works

--- src/main/scala/ru/kotletkin/Main.scala
+++ src/main/scala/ru/kotletkin/Main.scala
@@ -1,17 +1,44 @@
-package ru.kotletkin
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.lit
-
-object Main {
+object SparkStableApp {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().master("local[*]").getOrCreate()
 
-    // Считаем сумму чисел от 1 до 100
-    val df = spark.range(1, 5) // создает колонку "id"
-      .withColumn("status", lit("active")) // добавляет колонку со строкой
+    System.setProperty("hadoop.home.dir", "C:\\hadoop")
 
-    df.show()
+
+    val spark = SparkSession.builder()
+      .appName("StableSparkApp")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    // Чтение CSV с заголовками
+    val df: DataFrame = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("data/input.csv") // путь к файлу
+
+    // Пример обработки: группировка и агрегация
+    val result = df
+      .groupBy($"category") // группировка по категории
+      .agg(
+        count($"id").as("total_count"),
+        avg($"price").as("avg_price"),
+        sum($"amount").as("total_amount")
+      )
+      .orderBy($"total_amount".desc) // сортировка
+
+    // Вывод схемы и данных
+    df.printSchema()
+    result.show(10, truncate = false)
+
+    // Запись результата в Parquet
+    result.write
+      .mode("overwrite")
+      .csv("output.csv")
+
     spark.stop()
   }
 }
